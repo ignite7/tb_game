63f8b895f46517cecc3214d3ded754b5afdb890b
ignite7 <ignite21@protonmail.com>
2020-12-14 19:30:07 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// React
+import {Fragment} from 'react';
+
+// Components
+
+function App() {
+  return (
+    <Fragment>
+      <header className="header"></header>
+      <main className="main"></main>
+      <footer className="footer"></footer>
+    </Fragment>
+  );
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// React
+import ReactDOM from "react-dom";
+
+// App
+import App from "./App";
+
+// Css
+import './styles/style.css'
+
+// DOM
+ReactDOM.render(<App />, document.getElementById("root"));
